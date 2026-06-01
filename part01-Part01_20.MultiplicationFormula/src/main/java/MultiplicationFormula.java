4e46cbb273a8f8a69435bf80e910d8dec2290b54
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-01 14:29:22 +0200

    add part 01_20

--- part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
+++ part01-Part01_20.MultiplicationFormula/src/main/java/MultiplicationFormula.java
@@ -6,7 +6,14 @@ public class MultiplicationFormula {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int firstNumber = scanner.nextInt();
 
+        System.out.println("Give the second number:");
+        int secondNumber = scanner.nextInt();
+
+        int product = firstNumber * secondNumber;
+
+        System.out.println(firstNumber + " * " + secondNumber + " = " + product);
     }
 }
