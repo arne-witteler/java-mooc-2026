f49fa0c3e23cb636845cad2aff22210e8f60ad89
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-05 14:18:19 +0200

    add part 01_24

--- part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -6,6 +6,11 @@ public class SpeedingTicket {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Give speed:");
+        int eingabe = scanner.nextInt();
+
+        if (eingabe > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }
