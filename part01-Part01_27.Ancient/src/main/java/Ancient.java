65b859be26aeff42ae224428eba088bd34c57a84
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-13 08:15:52 +0200

    add part 01_27

--- part01-Part01_27.Ancient/src/main/java/Ancient.java
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -6,6 +6,12 @@ public class Ancient {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        int eingabe = scan.nextInt();
+
+        System.out.println("Give a year:");
+
+        if (eingabe < 2015) {
+            System.out.println("Ancient history!");
+        }
     }
 }
