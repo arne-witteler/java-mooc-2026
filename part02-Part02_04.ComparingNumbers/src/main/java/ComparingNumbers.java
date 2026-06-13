5cb21018a630d46f66d7c8f5d94a970330fa2402
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-13 10:46:55 +0200

    add part 02_04

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -6,5 +6,15 @@ public class ComparingNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int firstInt = Integer.valueOf(scanner.nextLine());
+        int secondInt = Integer.valueOf(scanner.nextLine());
+
+        if (firstInt > secondInt) {
+            System.out.println(firstInt + " is greater than " + secondInt);
+        } else if (firstInt < secondInt) {
+            System.out.println(firstInt + " is smaller than " + secondInt);
+        } else {
+            System.out.println(firstInt + " is equal to " + secondInt);
+        }
     }
 }
