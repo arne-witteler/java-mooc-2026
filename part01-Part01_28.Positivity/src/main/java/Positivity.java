b7d93609b1907a18556d5f20ad8f7938d8a4291d
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-13 08:20:17 +0200

    add part 01_28

--- part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -6,6 +6,14 @@ public class Positivity {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        int eingabe = scan.nextInt();
+
+        System.out.println("Give a number:");
+
+        if (eingabe <= 0) {
+            System.out.println("The number is not positive.");
+        } else {
+            System.out.println("The number is positive.");
+        }
     }
 }
