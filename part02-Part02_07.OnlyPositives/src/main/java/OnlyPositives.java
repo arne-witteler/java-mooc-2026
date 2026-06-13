0f3cb519a92d6a9886bb6a3a3050ecf3c49cd5c1
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-13 11:20:46 +0200

    add part 02_07

--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -6,5 +6,22 @@ public class OnlyPositives {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Give a number:");
+            int eingabe = scanner.nextInt();
+
+            if (eingabe < 0) {
+                System.out.println("Unsuitable number");
+            }
+
+            if (eingabe == 0) {
+                break;
+            }
+
+            if (eingabe > 0) {
+                System.out.println((eingabe * eingabe));
+            }
+        }
+
     }
 }
