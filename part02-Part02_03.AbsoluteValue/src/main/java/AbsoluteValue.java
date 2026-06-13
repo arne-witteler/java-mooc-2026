f8c7c7819556b6094a4385492057287b79766e3b
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-13 10:42:22 +0200

    add part 02_03

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,12 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int eingabe = Integer.valueOf(scanner.nextLine());
+
+        if (eingabe < 0) {
+            System.out.println((eingabe * -1));
+        } else {
+            System.out.println(eingabe);
+        }
     }
 }
