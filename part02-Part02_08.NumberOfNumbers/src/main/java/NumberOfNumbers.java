085968696ae165f6049d622a49210c17005cfd90
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-13 11:28:40 +0200

    add part 02_08

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -6,5 +6,18 @@ public class NumberOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int count = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int eingabe = scanner.nextInt();
+
+            if (eingabe == 0) {
+                break;
+            } else {
+                count++;
+            }
+        }
+        System.out.println("Number of numbers: " + count);
     }
 }
