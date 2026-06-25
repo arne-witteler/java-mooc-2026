f6203c92304196920fc28d241259063ed017770d
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-25 12:51:53 +0200

    add part 02_12

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,21 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double sum = 0;
+        int numberOfEingabe = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int eingabe = scanner.nextInt();
+
+            if (eingabe == 0) {
+                break;
+            } else {
+                sum += eingabe;
+                numberOfEingabe++;
+            }
+        }
+
+        System.out.println("Average of the numbers: " + (sum / numberOfEingabe));
     }
 }
