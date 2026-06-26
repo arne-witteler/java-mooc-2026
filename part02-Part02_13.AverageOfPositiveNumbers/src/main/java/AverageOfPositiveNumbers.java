b4ce768d88d7bdfcac82f97c5be2bb8268287a60
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-06-26 10:06:57 +0200

    add part 02_13

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,22 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double sum = 0;
+        int number = 0;
+
+        while (true) {
+            int eingabe = scanner.nextInt();
+
+            if (eingabe == 0 && sum == 0) {
+                System.out.println("Cannot calculate average");
+                break;
+            } else if (eingabe == 0 && sum != 0) {
+                System.out.println((sum / number));
+                break;
+            } else if (eingabe > 0) {
+                sum += eingabe;
+                number++;
+            }
+        }
     }
 }
